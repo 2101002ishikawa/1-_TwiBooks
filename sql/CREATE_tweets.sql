1818bc79c5ece1971ebf8cb3a715d6907543aec5
aso_石川和博 <116612230+2101002ishikawa@users.noreply.github.com>
2022-10-28 11:10:35 +0900

    sql変更
    インクリメント使えるようにしてます

--- sql/CREATE_tweets.sql
+++ sql/CREATE_tweets.sql
@@ -1,12 +1,9 @@
-CREATE TABLE(
-    tweets_id           VARCHAR(20)     NOT NULL AUTO_INCREMENT,
 CREATE TABLE tweets(
     tweets_id           INT(20)     NOT NULL AUTO_INCREMENT,
     mem_id              INT(8)      NOT NULL,
     tweets_contents     VARCHAR(200)    NOT NULL,
     tweets_date         DATE            NOT NULL,
     tweets_img          BLOB            NOT NULL,
-    shohin_id           VARCHAR(10)     NOT NULL,
     shohin_id           INT(10)     NOT NULL,
     good                INT(8)          NOT NULL,
     bad                 INT(8)          NOT NULL,
