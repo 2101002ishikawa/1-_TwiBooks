6123b82ab691196515361d8131230338c0910212
aso_石川和博 <116612230+2101002ishikawa@users.noreply.github.com>
2022-10-28 11:01:39 +0900

    sql変更
    インクリメント使えるようにしてます

--- sql/CREATE_buydetails.sql
+++ sql/CREATE_buydetails.sql
@@ -1,8 +1,6 @@
 CREATE TABLE buydetails(
-    buy_id          VARCHAR(20) NOT NULL,
     buy_id          INT(20) NOT NULL,
     buy_detailsid   INT(20)     NOT NULL,
-    shohin_id       VARCHAR(10) NOT NULL,
     shohin_id       INT(10) NOT NULL,
     shohin_count    INT(20)     NOT NULL,
     PRIMARY KEY(buy_id,buy_detailsid),
