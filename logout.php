85f785db2721faef6e3727e5706e60273f0b5efe
2101002ishikawa <2101002@s.asojuku.ac.jp>
2022-11-04 15:00:07 +0900

    石川1104-1500

--- logout.php
+++ logout.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <?php
@@ -13,5 +12,7 @@
         session_destroy();
         header('Location:login.php');
     ?>    
+
+    <link rel="stylesheet" href="css/style.css?v=2">
 </body>
 </html>
