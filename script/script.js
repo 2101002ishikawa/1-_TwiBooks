9eb6dbf4a41e79b2c57518f4783ca52a43abc511
2101005 <116612779+2101005@users.noreply.github.com>
2022-11-22 12:20:42 +0900

    qqq

--- script/script.js
+++ script/script.js
@@ -36,3 +36,11 @@ new Vue({
         }
     }
 });
+
+
+//ナビゲーションバーの検索ボタンのJS 開閉ボタンを押した時には
+$(".open-btn1").click(function () {
+    $(this).toggleClass('btnactive');//.open-btnは、クリックごとにbtnactiveクラスを付与＆除去。1回目のクリック時は付与
+    $("#search-wrap").toggleClass('panelactive');//#search-wrapへpanelactiveクラスを付与
+  $('#search-text').focus();//テキスト入力のinputにフォーカス
+});
