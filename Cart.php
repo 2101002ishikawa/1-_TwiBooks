dff814f3fc8ee394b42aec61fcd01e9e90c8bb11
PtMK2 <2101036@s.asojuku.ac.jp>
2022-11-25 12:26:51 +0900

    耕太郎最強

--- Cart.php
+++ Cart.php
@@ -8,19 +8,29 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <title>Cart</title>
+    <style>
+        body{
+            background-color: #e9e9e9;
+        }
+        .logincard{
+            border: 3px solid #000000;
+            border-radius: 15px;
+        }
+        .card{
+            border: 3px solid #000000;
+            border-radius: 15px;
+        }
+    </style>
     <?php
     require_once "DBManager";
     $db=new DBManager;
     $book=$db->getShohin($id);
     $bookkakaku=$book["shohin_kakaku"];
     $bookname=$book["shoin_mei"];
-    
-
-    
     ?>
 </head>
 <body>
-    <div class="text-center">
+    <div class="card ext-center">
         <h2>カート</h2>
         <div class="card offset-3 col-6">
             <p><?php=$bookname?></p> <!-- 本の名前 -->
@@ -29,7 +39,7 @@
             <br>
         </div>
         <p>小計 値段テスト</p>
-        <input type="submit" value="購入手続きへ">
+        <input type="submit" value="購入手続きへ" class="mb-3 btn">
     </div>
 
     <link rel="stylesheet" href="css/style.css?v=2">
