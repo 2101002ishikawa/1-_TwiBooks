1264d191077c90dac5d58c0c6503f7b6091c1570
PtMK2 <2101036@s.asojuku.ac.jp>
2022-11-25 12:10:22 +0900

    林田耕太郎仕事

---
+++ BookDetail2.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <title>BooDetail</title>
+    <style>
+        body{
+            background-color: #e9e9e9;
+        }
+        .logincard{
+            border: 3px solid #000000;
+            border-radius: 15px;
+        }
+    </style>
+    <?php
+        require_once "DBManager";
+        $db=new DBManager;
+        $bookimage = $db->getShohinImg($id);
+        echo "<img src=$image>";
+        $book= $db->getShohin($id);
+        $bookname = $book['shohin_mei'];
+        $booktwiter= $book['shohin_writer'];
+        $bookkakaku= $book['shohin_kakaku'];
+        $bookid=$book['shohin_id'];
+
+    ?>
+</head>
+<body>
+    <button onclick="location.href='./top.php'">トップページ</button>
+    <button onclick="location.href='./newMember.php'">会員登録</button>
+    <button onclick="location.href='./login.php'">ログイン</button>
+    <button onclick="location.href='./insertBook.php'">本の登録</button>
+    <button onclick="location.href='./insertTweet.php'">つぶやき投稿</button>
+    <br>
+    <div class="row">
+        <div class="card offset-3 col-6 text-center" style="padding-bottom:10%; ">
+       <h3 class="mt-5"><?php=$bookname?></h3>   <!-- 本の名前  -->
+       <?php=$bookwriter?>  <!-- 本の著者 -->
+        <?php echo "<img src=$bookimage>"?> <!-- 本の画像 -->
+        <br>
+        <input type="number" name="kosuu" class="mb-3 btn">
+        <form action="ここに遷移する場所" method="post">
+        <h4><?php=$bookkakaku?></h4>
+        <input type="button" name="cart" value="カートに入れる" class="mb-3 btn">
+        <br>
+        <input type="submit" name="buynow" value="今すぐ購入" class="mb-3 btn">
+    </form>
+     </div>
+
+ 
+</body>
+</html>
